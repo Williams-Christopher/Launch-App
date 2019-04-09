b339368cd6e03beaa7a1a7606b224b3570acf7be
Christopher Williams <cw@chriswillia.ms>
2019-04-09 18:03:27 -0500

    Implements an id to map marker object and clicks on launchList divs.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 let launchData; // JSON from LaunchLibrary
 let weatherData; // JS Object to hold a reference to a launch ID and its associated weather forecast
 let map; // GoogleMap
+let markerList = {};
 let mapLoaded = false;
 
 function setupLaunchList(launchData) {
@@ -10,7 +11,7 @@ function setupLaunchList(launchData) {
         console.log(launch);
         $('#js-launch-list').append(
             `
-            <div class="launch-list-item">
+            <div class="launch-list-item" >
             <h1>${launch.name}</h1>
             <p>${launch.missions[0] ? launch.missions[0].description : 'Mission description is unavailable.'}</p>
             <p>Launch window: <date>${launch.windowstart} to ${launch.windowend}</p>
@@ -20,18 +21,28 @@ function setupLaunchList(launchData) {
     });
 }
 
-function addMapMarkers(launchData) {
-    console.log(`mapLoaded: ${mapLoaded}`);
-    console.log(`launchData length: ${launchData.length}`);
+function setupMapMarkers(launchData) {
+    console.log(`mapLoaded: ${mapLoaded}`); // 'debugging' to make sure the map hs benn through init
+    console.log(`launchData length: ${launchData.length}`); // How many elements are in launchData
     // Per: https://developers.google.com/maps/documentation/javascript/earthquakes
+    // Loop through the elements of launchData, extract the pad locations, create LatLng objects, and add a marker to the map
     for(let i = 0; i < launchData.length; i++) {
-        let lat, lng, latLng;
-        lat = launchData[i].location.pads[0].latitude;
-        lng = launchData[i].location.pads[0].longitude;
-        console.log(lat, lng);
-        latLng = new google.maps.LatLng(lat, lng);
+        //let lat, lng, latLng;
+        let lat = launchData[i].location.pads[0].latitude;
+        let lng = launchData[i].location.pads[0].longitude;
+        //console.log(lat, lng);
+        let latLng = new google.maps.LatLng(lat, lng);
         let marker = new google.maps.Marker({position: latLng, map: map});
+        // Finally, create an object whose keys are launchData.id and value is a reference to the marker
+        markerList[launchData[i].id] = marker;
     }
+    console.log(markerList);
+}
+
+function setupClickListeners() {
+    $('.launch-list-item').click(event => {
+        console.log('Clicked a launchList item');
+    });
 }
 
 function setupApplication() {
@@ -46,14 +57,15 @@ function setupApplication() {
     .then(response => response.json())
     .then(rjson => {
         launchData = rjson.launches; // launches is the name of the array of launch elements
-        addMapMarkers(launchData);
+        setupMapMarkers(launchData);
         setupLaunchList(launchData);
+        setupClickListeners();
     });
 }
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('js-map-inner'), {center: {lat: 30.2672, lng: -97.7431}, zoom: 3});
-    map.style.position = 'fixed';
+    //map.style.position = 'fixed';
     mapLoaded = true;
 }
 
