3f2a515cd3d0024ef8dcdf77566d78e8c95c0771
Christopher Williams <cw@chriswillia.ms>
2019-04-16 20:48:46 -0500

    Final styling complete as well as code cleanup.

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 ## Find out what's going up!
 
-LaunchApp gives you a list of upcoming space launches and lets you learn more about them in multiple ways.
+LaunchApp gives you a list of upcoming space launches and lets you learn more about them.
 
 LaunchApp utilizes:
-- APIs
-  - LaunchLibrary => (https://launchlibrary.net/docs/1.4/api.html)
-  - Google Maps Platform - JavaScript API => (http://developers.google.com/maps/documentation/)
 - HTML 5
 - CSS 3
 - JavaScript
 - jQuery
+- APIs
+  - LaunchLibrary => (https://launchlibrary.net/docs/1.4/api.html)
+  - Google Maps Platform - JavaScript API => (http://developers.google.com/maps/documentation/)
 
 Visit the [Live application](https://williams-christopher.github.io/Launch-App/)
--- js/custom_map_control.js
+++ js/custom_map_control.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function mapZoomOutControl(controlDiv, map) {
-    // Set CSS for the control border
+    // Control border
     var controlUI = document.createElement('div');
     controlUI.style.backgroundColor = '#fff';
     controlUI.style.border = '2px solid #fff';
@@ -12,7 +12,7 @@ function mapZoomOutControl(controlDiv, map) {
     controlUI.title = 'Zoom out to world';
     controlDiv.appendChild(controlUI);
 
-    // Set CSS for the control interior
+    // Control interior
     var controlText = document.createElement('div');
     controlText.style.color = 'rgb(25,25,25)';
     controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
@@ -28,28 +28,28 @@ function mapZoomOutControl(controlDiv, map) {
 }
 
 function mapAboutControl(controlDiv, map) {
-        // Set CSS for the control border
-        var controlUI = document.createElement('div');
-        controlUI.style.backgroundColor = '#fff';
-        controlUI.style.border = '2px solid #fff';
-        controlUI.style.cursor = 'pointer';
-        controlUI.style.marginBottom = '22px';
-        controlUI.style.marginRight = '10px';
-        controlUI.style.textAlign = 'center';
-        controlUI.title = 'About LaunchApp';
-        controlDiv.appendChild(controlUI);
-    
-        // Set CSS for the control interior
-        var controlText = document.createElement('div');
-        controlText.style.color = 'rgb(25,25,25)';
-        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-        controlText.style.fontSize = '16px';
-        controlText.style.lineHeight = '38px';
-        controlText.style.paddingLeft = '5px';
-        controlText.style.paddingRight = '5px';
-        controlText.innerHTML = '<img class="btn-about-logo" src="./images/btn_about_logo.png" alt="LaunchApp Logo" />About LaunchApp';
-        controlUI.appendChild(controlText);
-    
-        // Add event listener for about button clicks
-        controlUI.addEventListener('click', () => toggleAboutSplashDisplay());
+    // Control border
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.marginRight = '10px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'About LaunchApp';
+    controlDiv.appendChild(controlUI);
+
+    // Control interior
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = '<img class="btn-about-logo" src="./images/btn_about_logo.png" alt="LaunchApp Logo" />About LaunchApp';
+    controlUI.appendChild(controlText);
+
+    // Add event listener for about button clicks
+    controlUI.addEventListener('click', () => toggleAboutSplashDisplay());
 }
